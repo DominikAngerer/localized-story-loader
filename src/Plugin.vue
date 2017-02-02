58b439a5846b0dd5b851538cff32bfd806bda6b9
Philipp Lupulescu <philipp.lupulescu@netural.com>
2017-02-02 15:18:21 +0100

    Sort by date

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,53 +1,56 @@
 <template>
-  <div class="uk-form-row" v-if="data.type == 'single'">
+  <div class="uk-form-row" v-if="type == 'single'">
     <select class="uk-width-1-1" v-model="model.value">
         <option></option>
-        <option v-for="loadedStory in data.loadedStories" v-bind:value="loadedStory.uuid">{{ loadedStory.name }}</option>
+        <option v-for="loadedStory in loadedStories" v-bind:value="loadedStory.uuid">{{ loadedStory.name }}</option>
       </select>
   </div>
-
   <div class="uk-form-row" v-else>
-    <label class="uk-margin-right uk-margin-bottom uk-display-inline-block" v-for="loadedStory in data.loadedStories">
-      <input v-model="model" type="checkbox" class="uk-margin-small-right" value="{{ loadedStory.uuid }}">
+    <label class="uk-margin-right uk-margin-bottom uk-display-inline-block" v-for="loadedStory in loadedStories">
+      <input v-model="model.values" type="checkbox" class="uk-margin-small-right" value="{{ loadedStory.uuid }}">
       {{ loadedStory.name }}
     </label>
   </div>
+  <input type="hidden" v-model="model.type" value="{{ type }}">
 </template>
 
 <script>
   export default {
-    plugin: 'localized-story-loaderV2',
+    plugin: 'localized-story-loader',
     mixins: [window.Storyblok.plugin],
-    data: {
-      loadedStories: [],
-      type: 'single'
+    data: function () {
+      return {
+        loadedStories: [],
+        type: 'single'
+      }
     },
     methods: {
       initWith: function () {
         return {
-          value: ''
+          value: '',
+          values: [],
+          type: ''
         }
       }
     },
     events: {
       'plugin:created': function () {
         var _locale = 'de';
-        if (this.$parent.$parent.$parent.$get('model') && this.$parent.$parent.$parent.$get('model').story) {
-          var _locale = this.$parent.$parent.$parent.$get('model').story.full_slug.slice(0, 2);
+        if (this.$store && this.$store.model && this.$store.model.story) {
+          var _locale = this.$store.model.story.full_slug.slice(0, 2);
         }
         if (['de', 'en'].indexOf(_locale) < 0) {
           _locale = 'de';
         }
         if (!this.schema.options) {
-          console.error('localized-story-loader: Define the following options: 0 : token, 1 : starts_with, 2: type (multi|single)');
+          console.error('localized-story-loader: Define the following options: 0 : token, 1 : starts_with, 2: type (single|multi)');
           return false;
         }
-        this.$set('data.type', this.schema.options[2].value);
+        this.$set('type', this.schema.options ? this.schema.options[2].value : 'single');
         jQuery.ajax({
-          url: 'https://api.storyblok.com/v1/cdn/stories/?token=INwFVCQIlV2S2bhX6ff28gtt&starts_with=news',
-          //url: 'https://api.storyblok.com/v1/cdn/stories/?token=' + this.schema.options[0].value + '&starts_with=' + _locale + '/' + this.schema.options[1].value + '/&is_startpage=false&time=' + Date.now(),
+          url: 'https://api.storyblok.com/v1/cdn/stories/?token=' + this.schema.options[0].value + '&starts_with=' + _locale + '/' + this.schema.options[1].value + '/&is_startpage=false&time=' + Date.now() +'&sort_by=name:asc',
           success: (response) => {
-            this.$set('data.loadedStories', response.stories);
+            this.$set('loadedStories', response.stories);
           }
         });
       },
